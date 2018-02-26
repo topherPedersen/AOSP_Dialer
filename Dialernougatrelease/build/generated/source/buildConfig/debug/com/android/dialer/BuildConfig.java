419d25c461b76072782678e84cb01ae54cf29838
topherpedersen <15feetaway>
2018-02-25 19:57:53 -0600

    Initial commit

---
+++ Dialernougatrelease/build/generated/source/buildConfig/debug/com/android/dialer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.android.dialer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.android.dialer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 30000;
+  public static final String VERSION_NAME = "3.00.00";
+}
